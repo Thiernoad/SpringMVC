a08f07ce46007c12556eb8056ba6f8311ad9503b
Thierno Amadou Diallo <thiernoadi@gmail.com>
2020-05-14 17:28:26 -0400

    spring_security1

---
+++ src/main/java/org/sid/springmvc/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.sid.springmvc.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+   @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//utilise un seul mot de passe par defaut qui utiliser au demarrage exp:54cbe6f5-0fe3-4c69-ae85-e9ca962ef9b6
+		//super.configure(auth);
+	 //comment chercher les utilisateur
+	   auth.inMemoryAuthentication().withUser("user1").password("{noop}1234").roles("USER");
+	   auth.inMemoryAuthentication().withUser("user2").password("{noop}1234").roles("USER");
+	   auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("USER","ADMIN");
+	   
+	}
+	
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	//super.configure(http);
+    	//maintenant pour authoriser l'authentification
+    	http.formLogin();
+    	http.authorizeRequests().anyRequest().authenticated();
+    }
+}
